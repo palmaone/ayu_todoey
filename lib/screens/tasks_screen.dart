3f7cd62f2ef99785cce91841d9a27f6551bb0fc7
Israel Palma <isra.palma@gmail.com>
2024-10-14 14:49:02 -0600

    Finished lesson 1 for this project, which is lesson 196 for Angela's Udemy flutter Course

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: const FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: null,
+        child: Icon(
+            Icons.add,
+            color: Colors.white,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 60.0, right: 30.0, bottom: 30.0, left: 30.0),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(height: 10.0,),
+                Text(
+                    'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
